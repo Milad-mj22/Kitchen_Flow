acdb0de781eb4bd0ea6aab1ad6a0e9974f885939
MIlad mj22 <m.moltaji@yahoo.com>
2025-03-17 14:21:13 +0330

    nothing

--- user_management/settings.py
+++ user_management/settings.py
@@ -37,9 +37,14 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['37.191.77.130','192.168.1.107','172.20.10.4','127.0.0.1','0.0.0.0','192.168.1.30','94.183.166.188','192.168.1.211','37.255.213.180']
+ALLOWED_HOSTS = ['37.191.77.130','192.168.1.107','172.20.10.4','127.0.0.1','0.0.0.0','192.168.1.30','94.183.166.188','192.168.1.211','37.255.213.180',"seketalamanager.ir", "www.seketalamanager.ir", 'go.microsoft.com',"*"]
 
 
+
+# CSRF_TRUSTED_ORIGINS = ["https://seketalamanager.ir"]
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 
 # Application definition
